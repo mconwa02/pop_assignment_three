93956ded804ad7e64389af6422af3a299e9821b6
unknown <mconwa02@dcs.bbk.ac.uk>
2018-01-14 10:23:28 +0000

    Adding convertToInts(problem)

--- sudoku.py
+++ sudoku.py
@@ -14,3 +14,15 @@ def convertToSets(problem):
             else:
                 problem[i][j] = {problem[i][j]}
     return problem
+
+def convertToInts(problem):
+    """Converts a 2D array of sets into 2D array of integers"""
+    for i in range(len(problem)):
+        for j in range(len(problem[i])):
+            if len(problem[i][j]) == 1:
+                problem[i][j] = list(problem[i][j])[0]
+            elif len(problem[i][j]) == 0:
+                problem[i][j] = 0
+            else:
+                problem[i][j] = 0
+    return problem          
