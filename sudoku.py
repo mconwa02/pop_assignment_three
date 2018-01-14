ec778b0e4b58527aab34cdce375c65493fd74e3d
unknown <mconwa02@dcs.bbk.ac.uk>
2018-01-14 10:11:09 +0000

    Adding convertToSets(problem) function

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,16 @@
 def read_sudoku(file):
-    """haven't told how to read from files """
+    """reads in files"""
     stream = open(file)
     data = stream.readlines()
     stream.close()
     return eval("".join(data))
+
+def convertToSets(problem):
+    """Converts a 2D array of integers into a 2D array of sets"""
+    for i in range(len(problem)):
+        for j in range(len(problem[i])):
+            if problem[i][j] == 0:
+                problem[i][j] = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+            else:
+                problem[i][j] = {problem[i][j]}
+    return problem
