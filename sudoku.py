166480528b52b4d423b73bb069cb7d1dfa9419b3
unknown <mconwa02@dcs.bbk.ac.uk>
2018-01-14 10:36:11 +0000

    Adding getBoxLocations(location) function

--- sudoku.py
+++ sudoku.py
@@ -40,3 +40,44 @@ def getColumnLocations(columnNumber):
     rows = list(range(0, 9))
     column = [columnNumber]*9
     return list(zip(rows, column))    
+
+
+def getBoxLocations(location):
+    """Return a list of all nine "locations"
+        (row, column) tuples in the same box as the given location """
+    if location[0] in [0,1,2] and location[1] in [0,1,2]:
+        s = [0]*3+[1]*3+[2]*3
+        t = [0,1,2]*3
+        return list(zip(s, t))
+    if location[0] in [0,1,2] and location[1] in [3,4,5]:
+        s = [0]*3+[1]*3+[2]*3
+        t = [3,4,5]*3
+        return list(zip(s, t)) 
+    if location[0] in [0,1,2] and location[1] in [6,7,8]:
+        s = [0]*3+[1]*3+[2]*3
+        t = [6,7,8]*3
+        return list(zip(s, t))
+    if location[0] in [3,4,5] and location[1] in [0,1,2]:
+        s = [3]*3+[4]*3+[5]*3
+        t = [0,1,2]*3
+        return list(zip(s, t))
+    if location[0] in [3,4,5] and location[1] in [3,4,5]:
+        s = [3]*3+[4]*3+[5]*3
+        t = [3,4,5]*3
+        return list(zip(s, t))
+    if location[0] in [3,4,5] and location[1] in [6,7,8]:
+        s = [3]*3+[4]*3+[5]*3
+        t = [6,7,8]*3
+        return list(zip(s, t))
+    if location[0] in [6,7,8] and location[1] in [0,1,2]:
+        s = [6]*3+[7]*3+[8]*3
+        t = [0,1,2]*3
+        return list(zip(s, t))
+    if location[0] in [6,7,8] and location[1] in [3,4,5]:
+        s = [6]*3+[7]*3+[8]*3
+        t = [3,4,5]*3
+        return list(zip(s, t))
+    if location[0] in [6,7,8] and location[1] in [6,7,8]:
+        s = [6]*3+[7]*3+[8]*3
+        t = [6,7,8]*3
+        return list(zip(s, t))
